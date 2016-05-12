6df656f0c22d7bb4cad72b6f6cbc45407196ab67
djuke <dvh@xdh.nl>
2016-05-12 12:59:22 +0200

    reading the aws instance it was assumed that eth0 was the first in the list of network interfaces

--- builtin/providers/aws/resource_aws_instance.go
+++ builtin/providers/aws/resource_aws_instance.go
@@ -495,8 +495,12 @@ func resourceAwsInstanceRead(d *schema.ResourceData, meta interface{}) error {
 	d.Set("iam_instance_profile", iamInstanceProfileArnToName(instance.IamInstanceProfile))
 
 	if len(instance.NetworkInterfaces) > 0 {
-		d.Set("subnet_id", instance.NetworkInterfaces[0].SubnetId)
-		d.Set("private_ips", flattenInstancePrivateIpAddresses(instance.NetworkInterfaces[0].PrivateIpAddresses))
+		for _, ni := range instance.NetworkInterfaces {
+			if *ni.Attachment.DeviceIndex == 0 {
+				d.Set("subnet_id", ni.SubnetId)
+				d.Set("private_ips", flattenInstancePrivateIpAddresses(ni.PrivateIpAddresses))
+			}
+		}
 	} else {
 		d.Set("subnet_id", instance.SubnetId)
 	}
